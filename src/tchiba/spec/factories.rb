76f34d19546fbc8785badec6acc9f9f927f054a0
Nick Ducker <nick_ducker@live.com>
2020-07-28 15:18:02 +0930

    add factories for addresses and users

---
+++ src/tchiba/spec/factories.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+    email { "them@test.com" }
+    password  { "password" }
+  end
+
+  factory :address do
+    user_id { 1 }
+    number  { 99 }
+    street { "Test St" }
+    suburb { "Suburbia" }
+    state { "SA" }
+    postcode { 9999 }
+  end
+end
