cb7c7cc0eca463be49bc18c6a8e08e0be5425b02
Nick Ducker <nick_ducker@live.com>
2020-07-28 13:39:36 +0930

    Add user field validation testing

--- src/tchiba/spec/models/user_spec.rb
+++ src/tchiba/spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject {
+    described_class.new(id: 1,
+                        email: "test@test.com",
+                        password: "password")
+  }
+
+  it "is valid with valid attributes" do 
+    expect(subject).to be_valid
+  end
+
+  it "is valid without a name" do 
+    subject.name = nil
+    expect(subject).to be_valid
+  end
+
+  it "is valid without a bio" do
+    subject.bio = nil
+    expect(subject).to be_valid    
+  end
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without credit" do
+    subject.credit = nil
+    expect(subject).to_not be_valid
+  end
+
 end
