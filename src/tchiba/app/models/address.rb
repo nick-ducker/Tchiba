205038dc525287105d227556b6f4352fafd5d48f
Nick Ducker <nick_ducker@live.com>
2020-07-28 15:18:26 +0930

    green address test

--- src/tchiba/app/models/address.rb
+++ src/tchiba/app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+
+  belongs_to :user
+
+  validates_presence_of :number, :street, :suburb, :state, :postcode
+  validates_numericality_of :number, :postcode
+
 end
--- src/tchiba/spec/models/address_spec.rb
+++ src/tchiba/spec/models/address_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    build(:user)
+    @address = build(:address)
+  end
+
+  it "is valid with valid attributes" do 
+    expect(@address).to be_valid
+  end
+
+  it "is not valid without a number that is an integer" do
+    @address.number = nil
+    expect(@address).to_not be_valid
+  end
+
+  it "is not valid without a street" do
+    @address.street = nil
+    expect(@address).to_not be_valid
+  end
+
+  it "is not valid without a suburb" do
+    @address.suburb = nil
+    expect(@address).to_not be_valid
+  end
+
+  it "is not valid without a state" do
+    @address.state = nil
+    expect(@address).to_not be_valid
+  end
+
+  it "is not valid without postcode that is an integer" do
+    @address.postcode = nil
+    expect(@address).to_not be_valid
+  end
+
+  it { should belong_to(:user).without_validating_presence }
+
 end
