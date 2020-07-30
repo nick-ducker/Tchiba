5f3df89b9e64ff1ade2da5a6caf1ffd23f836e39
Nick Ducker <nick_ducker@live.com>
2020-07-30 13:57:37 +0930

    add routes, controller actions and views for pages controller

--- src/tchiba/app/controllers/pages_controller.rb
+++ src/tchiba/app/controllers/pages_controller.rb
@@ -1,2 +1,15 @@
 class PagesController < ApplicationController
+
+  def home
+  end
+
+  def contact
+  end
+
+  def admindashboard
+  end
+
+  def browse
+  end
+
 end
--- src/tchiba/config/routes.rb
+++ src/tchiba/config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
+
+  get '/', to: 'pages#home'
+  get '/home', to: 'pages#home'
+
+  get '/contact', to: 'pages#contact'
+  get '/admindash', to: 'pages#admindashboard'
+
+  get '/browse', to: 'pages#browse'
+
 end
