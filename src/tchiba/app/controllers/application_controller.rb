b85b9c96988c8b538ea38eb703edb261cd7067e1
Nick Ducker <nick_ducker@live.com>
2020-07-31 14:07:59 +0930

    add authentication method to application controller

--- src/tchiba/app/controllers/application_controller.rb
+++ src/tchiba/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def authenticate 
+    unless current_user
+      flash[:alert] = "You must be logged in to view this page"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
 end
