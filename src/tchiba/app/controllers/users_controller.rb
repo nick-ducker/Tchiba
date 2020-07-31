9c607fa2ff3b7c91ff79f12bc33baf4bb97d1fa7
Nick Ducker <nick_ducker@live.com>
2020-07-31 15:08:42 +0930

    add profile and show actions to user controller

--- src/tchiba/app/controllers/users_controller.rb
+++ src/tchiba/app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate, only: [:show, :reviewhistory, :ordertransactionhistory]
+
   def show
     #private
+    @user = current_user
   end
 
   def profile
-    #public
+    begin
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "That user doesn't exist"
+      redirect_to root_path
+    end
   end
 
   def reviewhistory
@@ -14,4 +22,6 @@ class UsersController < ApplicationController
   def ordertransactionhistory
     #private
   end
+
+  private
 end
