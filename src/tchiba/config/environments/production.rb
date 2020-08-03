8f0c3983897d9413df27ac723e2a82745111adf1
Nick Ducker <nick_ducker@live.com>
2020-08-03 15:20:41 +0930

    production environment update for gmail

--- src/tchiba/config/environments/production.rb
+++ src/tchiba/config/environments/production.rb
@@ -122,13 +122,12 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.perform_deliveries = true
   ActionMailer::Base.raise_delivery_errors = true
-  ActionMailer::Base.smtp_settings = 
-  {
+  ActionMailer::Base.smtp_settings = {
     :address            => 'smtp.gmail.com',
     :port               => 587,
     :domain             => 'gmail.com', 
     :authentication     => :plain,
-    :user_name          => 'gcam021932@coderacademy.edu.au',
-    :password           => '12345678'
+    :user_name          => 'tchiba.app@gmail.com',
+    :password           => ENV['GMAIL_PASSWORD']
   }
 end
