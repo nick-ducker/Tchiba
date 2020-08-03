e9cb8efb6a76e727f17ca9ed799963118256d153
Nick Ducker <nick_ducker@live.com>
2020-08-03 11:26:17 +0930

    change production env for mailgun

--- src/tchiba/config/environments/production.rb
+++ src/tchiba/config/environments/production.rb
@@ -110,6 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  config.action_mailer.default_url_options = { host: 'sandboxf9ff4e723e1440359581e7991ff13ba4.mailgun.org' }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
       api_key: Rails.application.credentials.dig(:mailgun, :api_key),
