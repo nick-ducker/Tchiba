a8fab4be957c533f9466e19bec2a4afefe44498b
Nick Ducker <nick_ducker@live.com>
2020-08-03 10:17:48 +0930

    add v2 seed file

--- src/tchiba/db/seeds.rb
+++ src/tchiba/db/seeds.rb
@@ -9,20 +9,27 @@
 n = 1
 user_id = n
 
-email = "email#{n}"
-encrypted_password = "password#{n}"
-name = "user#{n}"
-bio = "I am a bio for #{name}"
+4.times do
+  email = "email#{n}@email.com"
+  encrypted_password = "password#{n}"
+  name = "user#{n}"
+  bio = "I am a bio for #{name}"
+  user = User.new(email: email, password: encrypted_password, name: name, bio: bio)
+  user.skip_confirmation!
+  user.save!
+  n += 1
+end
 
-number = n
-street = "street #{n}"
-suburb = "suburb #{n}"
-state = "state #{n}"
-postcode = n + 5000
+n = 1
 
-4.times do 
-  user = User.create(email: email, encrypted_password: encrypted_password, name: name, bio: bio)
-  user.create_address(number: number, user_id: user_id, street: street, suburb: suburb, state: state, postcode: postcode)
+4.times do
+  number = n
+  street = "street #{n}"
+  suburb = "suburb #{n}"
+  state = "state #{n}"
+  postcode = n + 5000
+  user = User.find(n)
+  user.create_address(number: number, street: street, suburb: suburb, state: state, postcode: postcode)
   user.create_cart
   n += 1
 end
@@ -30,14 +37,13 @@ end
 n = 1
 x = 1
 
-name = "blend#{n}"
-price = n
-quantity = n + 10
-descrip = "This is a description"
-weight = 500
-
 8.times do
-  User.find(x).blend.create(name: name, price: price, quantity: quantity, descrip: descrip, weight: weight)
+  name = "blend#{n}"
+  price = n
+  quantity = n + 10
+  descrip = "This is a description"
+  weight = 500
+  User.find(x).blends.create(name: name, price: price, quantity: quantity, descrip: descrip, weight: weight)
   if n % 2 == 0
     x += 1
     n += 1
@@ -50,13 +56,13 @@ carts = Cart.all
 
 carts.each do |cart|
   3.times do
-    cart.cart_item.create(blend_id: rand(1..8), quantity: rand(1..5))
+    cart.cart_items.create(blend_id: rand(1..8), blend_quantity: rand(1..5))
   end
 
-  cart.cart_item.each do |cartitem|
-    gross = cart_item.blend.price * cart_item.quantity
+  cart.cart_items.each do |cart_item|
+    gross = cart_item.blend.price * cart_item.blend_quantity
     shipping = cart_item.blend.shipping_cost
-    discount = cart_item.blend.discount * cart_item.quantity
+    discount = cart_item.blend.discount_amount * cart_item.blend_quantity
     total = (gross + shipping) - discount
     cart_item.create_order(buyer_id: cart_item.cart.user_id, seller_id: cart_item.blend.user_id, gross: gross, total: total, discount: discount )
   end
@@ -66,5 +72,5 @@ end
 orders = Order.all
 
 orders.each do |order|
-  order.transaction.create(amount: order.total, paid:true)
+  order.transactions.create(amount: order.total, paid:true)
 end
