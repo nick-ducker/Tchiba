e286d71cd1e674cd44531c5baa4ac279ce1797fd
Nick Ducker <nick_ducker@live.com>
2020-08-03 13:34:41 +0930

    update devise initializer for mailgun

--- src/tchiba/config/initializers/devise.rb
+++ src/tchiba/config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'mailgun@sandboxf9ff4e723e1440359581e7991ff13ba4.mailgun.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
