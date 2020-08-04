ed33eede6bb123453def838e5ab71a322ca656a0
Nick Ducker <nick_ducker@live.com>
2020-08-04 09:36:44 +0930

    add remove item from cart and dynamic rendering of show depending on cart item status

--- src/tchiba/app/controllers/carts_controller.rb
+++ src/tchiba/app/controllers/carts_controller.rb
@@ -3,12 +3,19 @@ class CartsController < ApplicationController
 
   def show
     @cartitems = current_user.cart.cart_items
+    n = 0
+    @cartitems.each{|x| x.order ? n += 1 : nil}
+    n == @cartitems.size ? (@notable = true) : (@notable = false)
   end
 
   def update_quantity
   end
 
   def remove_item
+    cartitem = CartItem.find(params[:id])
+    cartitem.destroy
+    flash[:alert] = "Blend removed from cart"
+    redirect_back(fallback_location: root_path)
   end
 
   def checkout
