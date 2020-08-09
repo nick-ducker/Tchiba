806dc9b432227e84881c236c8ad0d828e6a88dc7
Nick Ducker <nick_ducker@live.com>
2020-08-09 13:20:59 +0930

    add improved alerts

--- src/tchiba/app/helpers/application_helper.rb
+++ src/tchiba/app/helpers/application_helper.rb
@@ -2,13 +2,4 @@ module ApplicationHelper
 
   include Pagy::Frontend
 
-  def flash_class(level)
-    case level
-        when :notice then "alert alert-info"
-        when :success then "alert alert-success"
-        when :error then "alert alert-error"
-        when :alert then "alert alert-error"
-    end
-  end
-
 end
