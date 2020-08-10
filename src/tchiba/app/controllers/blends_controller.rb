0741f6f4cfd493fe16e942d7a352a1f6161d34cc
Nick Ducker <nick_ducker@live.com>
2020-08-10 11:51:54 +0930

    blend controller refactor

--- src/tchiba/app/controllers/blends_controller.rb
+++ src/tchiba/app/controllers/blends_controller.rb
@@ -1,5 +1,6 @@
 class BlendsController < ApplicationController
   before_action :authenticate, only: [:create, :edit, :destroy, :update, :new]
+  before_action :set_blend, only: [:show, :edit, :destroy, :update]
 
   def create
     if current_user.blends.create(strong_blend_params)
@@ -15,12 +16,10 @@ class BlendsController < ApplicationController
   end
 
   def show
-    @blend = Blend.find(params[:id])
     @pagy, @reviews = pagy(@blend.reviews)
   end
 
   def edit
-    @blend = Blend.find(params[:id])
   end
 
   def delete_image
@@ -31,14 +30,12 @@ class BlendsController < ApplicationController
   end
 
   def destroy
-    blend = Blend.find(params[:id])
-    blend.destroy
+    @blend.destroy
     flash[:alert] = "Blend deleted."
     redirect_to root_path
   end
 
   def update
-    @blend = Blend.find(params[:id])
     if @blend.update(strong_blend_params)
       redirect_to blend_path(@blend)
     else
@@ -51,6 +48,10 @@ private
 
   def strong_blend_params
     params.require(:blend).permit(:name, :price, :shipping_cost, :discount_amount, :quantity, :descrip, :weight, property_ids: [], image: [])
+  end
+
+  def set_blend
+    @blend = Blend.find(params[:id])
   end
 
 end
