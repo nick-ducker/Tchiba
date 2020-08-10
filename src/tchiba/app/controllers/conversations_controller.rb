3311e4091ec3b01e209083a9e067e8d6b7801a5f
Nick Ducker <nick_ducker@live.com>
2020-08-10 10:42:27 +0930

    conversation control refactor

--- src/tchiba/app/controllers/conversations_controller.rb
+++ src/tchiba/app/controllers/conversations_controller.rb
@@ -1,30 +1,26 @@
 class ConversationsController < ApplicationController
   before_action :authenticate
+  before_action :set_conversation, only: [:show, :create, :add, :inactive, :destroy]
   
   def index
-    @to_conversations = Conversation.where(to_user_id: current_user.id)
-    @from_conversations = Conversation.where(from_user_id: current_user.id)
+    @to_conversations = current_user.to_conversations
+    @from_conversations = current_user.from_conversations
   end
 
   def show
-    @conversation = Conversation.find(params[:id])
     @message = Message.new
   end
 
   def new
     @blend = Blend.find(params[:blend_id])
-
     if Conversation.find_by(blend_id: @blend.id, from_user_id: current_user.id)
       redirect_to conversation_path(Conversation.find_by(blend_id: @blend.id, from_user_id: current_user.id)) and return
     end
-    
     @conversation = Conversation.create(to_user_id: @blend.user.id, from_user_id: current_user.id, blend_id: @blend.id)
-
     @message = Message.new
   end
 
   def create
-    @conversation = Conversation.find(params[:id])
     if @conversation.messages.create(strong_message_params)
       redirect_to conversation_path(@conversation)
     else
@@ -34,7 +30,6 @@ class ConversationsController < ApplicationController
   end
 
   def add
-    @conversation = Conversation.find(params[:id])
     unless @conversation.is_active
       flash[:alert] = "Cannot send message as this conversation is inactive"
       redirect_to conversation_path(@conversation) and return
@@ -48,29 +43,25 @@ class ConversationsController < ApplicationController
     end
   end
 
-  def inactive
-    conversation = Conversation.find(params[:id])
-    
-    if conversation.to_user_id != current_user.id
+  def inactive    
+    if @conversation.to_user_id != current_user.id
       flash[:alert] = "You don't have permission to do this"
       redirect_to conversation_path(@conversation) and return
     end
 
-    conversation.update(is_active: false)
+    @conversation.update(is_active: false)
     flash[:alert] = "Conversation is no longer active"
-    redirect_to conversation_path(conversation)
+    redirect_to conversation_path(@conversation)
   end
 
   def destroy
-    conversation = Conversation.find(params[:id])
-
-    if conversation.to_user_id != current_user.id
+    if @conversation.to_user_id != current_user.id
       flash[:alert] = "You don't have permission to do this"
       redirect_to conversation_path(@conversation) and return
     end
 
-    conversation.messages.destroy_all
-    conversation.destroy
+    @conversation.messages.destroy_all
+    @conversation.destroy
     flash[:alert] = "Conversation deleted"
     redirect_to conversations_path
   end
@@ -86,5 +77,7 @@ private
   end
 
   def set_conversation
+    @conversation = Conversation.find(params[:id])
   end
+  
 end
