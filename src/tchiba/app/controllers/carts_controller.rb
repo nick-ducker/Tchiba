a09bdc9964269e7095e2e580fab337a32cc4ab60
Nick Ducker <nick_ducker@live.com>
2020-08-10 12:07:14 +0930

    Comment cart controller

--- src/tchiba/app/controllers/carts_controller.rb
+++ src/tchiba/app/controllers/carts_controller.rb
@@ -4,6 +4,8 @@ class CartsController < ApplicationController
   after_action :cart_count, only: [:update_quantity, :add_item, :remove_item]
 
   def show
+    #Find cart items for a particular user by using the devise current_user method to pull the current user. 
+    #Authentication ensures this action will only be accessed by a logged in user
     @cartitems = current_user.cart.cart_items
   end
 
@@ -14,6 +16,7 @@ class CartsController < ApplicationController
   end
 
   def remove_item
+    #Checks the cart item to see if it has an order attached to it
     if @cartitem.order
       flash[:alert] = "You must cancel your order before you can remove this item from your cart"
       redirect_to order_path(@cartitem.order)
@@ -25,6 +28,7 @@ class CartsController < ApplicationController
   end
 
   def add_item
+    #Checks the current users cart items to see if they already have a cart item for the blend they're trying to add to their cart.
     if current_user.cart.cart_items.find_by(blend_id: strong_cart_params[:blend_id])
       flash[:alert] = "You already have this item in your cart"
       redirect_back(fallback_location: root_path)
@@ -42,6 +46,7 @@ private
   end
 
   def set_cart_item
+    #Find the cart item using the send through parameter ID
     @cartitem = CartItem.find(params[:id])
   end
 
