4bb067426fc3781442902428f15fb6be77ff9563
Nick Ducker <nick_ducker@live.com>
2020-08-10 12:14:20 +0930

    conversation controller comments

--- src/tchiba/app/controllers/conversations_controller.rb
+++ src/tchiba/app/controllers/conversations_controller.rb
@@ -3,6 +3,7 @@ class ConversationsController < ApplicationController
   before_action :set_conversation, only: [:show, :create, :add, :inactive, :destroy]
   
   def index
+    #Find the to and from conversations by pulling them directly from the current users to_conversations and from_conversations relationships.
     @to_conversations = current_user.to_conversations
     @from_conversations = current_user.from_conversations
   end
@@ -12,12 +13,18 @@ class ConversationsController < ApplicationController
   end
 
   def new
+    #Find the blend by the sent through blend_id in parameters
     @blend = Blend.find(params[:blend_id])
+
+    #Only a non-owning user can start a conversation, so before creating a new conversation, first check the db for a conversation with a matching blend_if and from_user_id
+    #and redirect there if it exists.
     if Conversation.find_by(blend_id: @blend.id, from_user_id: current_user.id)
-      redirect_to conversation_path(Conversation.find_by(blend_id: @blend.id, from_user_id: current_user.id)) and return
+      redirect_to conversation_path(Conversation.find_by(blend_id: @blend.id, from_user_id: current_user.id))
+    else
+      @conversation = Conversation.create(to_user_id: @blend.user.id, from_user_id: current_user.id, blend_id: @blend.id)
+      @message = Message.new
     end
-    @conversation = Conversation.create(to_user_id: @blend.user.id, from_user_id: current_user.id, blend_id: @blend.id)
-    @message = Message.new
+    
   end
 
   def create
@@ -44,26 +51,30 @@ class ConversationsController < ApplicationController
   end
 
   def inactive    
+    #Check the set conversation to user against the current user id to authenticate action
     if @conversation.to_user_id != current_user.id
       flash[:alert] = "You don't have permission to do this"
-      redirect_to conversation_path(@conversation) and return
+      redirect_to conversation_path(@conversation)
+    else
+      @conversation.update(is_active: false)
+      flash[:alert] = "Conversation is no longer active"
+      redirect_to conversation_path(@conversation)
     end
 
-    @conversation.update(is_active: false)
-    flash[:alert] = "Conversation is no longer active"
-    redirect_to conversation_path(@conversation)
+    
   end
 
   def destroy
+    #Check the set conversation to user against the current user id to authenticate action
     if @conversation.to_user_id != current_user.id
       flash[:alert] = "You don't have permission to do this"
-      redirect_to conversation_path(@conversation) and return
-    end
-
-    @conversation.messages.destroy_all
-    @conversation.destroy
-    flash[:alert] = "Conversation deleted"
-    redirect_to conversations_path
+      redirect_to conversation_path(@conversation) 
+    else
+      @conversation.messages.destroy_all
+      @conversation.destroy
+      flash[:alert] = "Conversation deleted"
+      redirect_to conversations_path
+    end    
   end
 
 private
@@ -77,7 +88,8 @@ private
   end
 
   def set_conversation
+    #Find a conversation using the send parameter ID
     @conversation = Conversation.find(params[:id])
   end
-  
+
 end
